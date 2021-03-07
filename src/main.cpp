f54dfb39723d3d7ecf414ef0c5eb8150fbc3d7df
Jorge15042001 <adrivulgarin@hotmail.com>
2021-03-06 22:21:27 -0500

    first fully functional unbeatable bot

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,92 @@
+#include <cstdint>
 #include <iostream>
+#include <string.h>
+
 #include "tic-tac-toe.h"
-int main()
+const std::uint16_t getPlayerPos(const  game_state& gs)
 {
+    std::uint16_t r;
+    std::uint16_t c;
+
+    std::cout<<"Input row and col: \nRow: ";
+    std::cin>>r;
+    std::cout<<"Col: ";
+    std::cin>>c;
+    const  std::uint16_t pos= r+r+r+c;
+    std::cout<<"position result <"<<r<<","<<c<<"> pos: "<<pos<<'\n';
+    if(/*pos >=0 && pos<9&&*/gs.is_playeble_movement(pos))return pos;
+    std::cout<<"Not available movement\n";
+    return getPlayerPos(gs);
+}
+void printHelp(const char * filename)
+{
+    std::cout<<filename<<" -help\tshow help\n";
+    std::cout<<filename<<" -Human\tfirst movement done by human\n";
+    std::cout<<filename<<" -Computer\tfirst movement done by Computer\n";
+
+
+}
+int main(const int argc, const char *const* const argv)
+{
+    
+    if (argc!=2){
+        std::cerr<<"Only 1 argument must be passed\n";
+        printHelp(argv[0]);
+        return -1;
+    }
+    game_state * actual_game= new game_state(0,0,Player_t::Computer,0);
+    if (strcmp(argv[1],"-help")==0)
+    {
+        printHelp(argv[0]);
+        return 0;
+    }
+    else if(strcmp(argv[1],"-Human")==0)
+    {
+        delete actual_game;
+        actual_game= new game_state(0,0,Player_t::Human,0);
+    }
+    else if(strcmp(argv[1],"-Computer")==0)
+    {
+            
+    }
+    else 
+    {
+        std::cout<<argv[1]<<" Not recognice argument\n";
+        printHelp(argv[0]);
+        return -1;
+    }
     /*
     Player_t a=Player_t::Computer;
     std::cout<<a<<'\n';
     */
     //const short computer =1 |1<<2 |1<<5;
     //const short player= 2| 1<<3|1<<6;
-    game_state g(0,0,Player_t::Computer,0);
-    g.compute_stats();
-    std::cout<<g;
-    while(g.winner==Player_t::NoWiner)
-    {
-        std::cout<<"COMPUTER\n";
-        g=g.optimal_movement();
-        std::cout<<g;
-        /*
-        std::cout<<"Ingese la fila"<<'\n';
-        int f;
-        std::cin>>f;
-        
-        std::cout<<"Ingese la col"<<'\n';
-        int c;
-        std::cin>>c;
-        int pos=f*3+c;
-        for (auto & pgs:g.possible_game_states)
-        {
-            if (pgs.selected_movement==pos)g=pgs;
-        }
-        std::cout<<g;
-        */
-    }
-    
+    actual_game->compute_stats();
+    //game_state new_game = g.optimal_movement();
 
+    std::cout<<*actual_game;
+   // 
+    while(actual_game->winner==Player_t::NoWiner)
+    {
+        if (actual_game->actual_player==Player_t::Computer)
+        {
+            std::cout<<"COMPUTER\n";
+            actual_game=actual_game->optimal_movement();
+            std::cout<<*actual_game;
+        }
+        else
+        {
+            const std::uint16_t pos=getPlayerPos(*actual_game);
+            for (auto & pgs:actual_game->possible_game_states)
+            {
+                if (pgs.selected_movement==pos)
+                {
+                    actual_game=&pgs;
+                }
+            }
+            std::cout<<"Human\n";
+            std::cout<<*actual_game;
+        }
+    }
 
 }
--- src/tic-tac-toe.h
+++ src/tic-tac-toe.h
@@ -1,6 +1,7 @@
 //engine for playing tic-tac-toe
 //matrix game layout
 //0 1 2
+//
 //0 1 1
 //2 1 2
 //0 - reprensents empty space
@@ -11,6 +12,7 @@
 //payer1 = 0b010011010
 //payer2 = 0b001000101
 #include <bitset>
+#include <cstdint>
 #include <iostream>
 #include <utility>
 #include <vector>
@@ -44,26 +46,26 @@ std::ostream& operator<<(std::ostream& o, const Player_t &value)
 }
 struct game_state
 {
-    const unsigned short computer;
-    const unsigned short player;
+    const std::uint16_t computer;
+    const std::uint16_t player;
     const Player_t actual_player;
     Player_t  winner;
-    const unsigned char selected_movement;
+    const std::uint8_t selected_movement;
     std::vector<game_state> possible_game_states;
     long win_computer=0;
     long win_human=0;
     long tie=0;
 
-    game_state &operator =( game_state &other)
+    game_state operator =( game_state other)
     {
         return other;
 
     }
 
-    game_state( const unsigned short computer,
-                const unsigned short player, 
+    game_state( const std::uint16_t computer,
+                const std::uint16_t player, 
                 const Player_t next_movement,
-                const  unsigned char selected_movement):
+                const  std::uint8_t selected_movement):
         computer(computer),
         player(player),
         actual_player(next_movement),
@@ -75,14 +77,14 @@ struct game_state
     }
 
     const Player_t compute_winer(){
-        static const unsigned short mask_all_row=1|2|4;//0b0000000111 
-        static const unsigned short mask_all_col=1|1<<3|1<<6;//0b000001001001
-        static const unsigned short mask_diagonal1=1|1<<4|1<<8;
-        static const unsigned short mask_diagonal2=1<<2|1<<4|1<<6;
+        static const std::uint16_t mask_all_row=1|2|4;//0b0000000111 
+        static const std::uint16_t mask_all_col=1|1<<3|1<<6;//0b000001001001
+        static const std::uint16_t mask_diagonal1=1|1<<4|1<<8;
+        static const std::uint16_t mask_diagonal2=1<<2|1<<4|1<<6;
         for (int i=0;i<3;i++)//check allrows and cols
         {
-            const unsigned short mask_row_shifted=mask_all_row<<(3*i);
-            const unsigned short mask_col_shifted=mask_all_col<<i;
+            const std::uint16_t mask_row_shifted=mask_all_row<<(3*i);
+            const std::uint16_t mask_col_shifted=mask_all_col<<i;
 
             if((computer&mask_row_shifted)==mask_row_shifted)return Player_t::Computer;
             if((computer&mask_col_shifted)==mask_col_shifted)return Player_t::Computer;
@@ -125,9 +127,9 @@ struct game_state
         
     }
 
-    bool is_playeble_movement(const unsigned char  pos) const
+    bool is_playeble_movement(const std::uint8_t  pos) const
     {
-       unsigned short  mask=1<<pos;
+       std::uint16_t  mask=1<<pos;
        return (~computer&mask)&(~player&mask);//if not of the bits at pos are set
     }
     
@@ -140,9 +142,9 @@ struct game_state
             if(is_playeble_movement(i) && winner==Player_t::NoWiner)
             {
                 //compute the next movement state if  i is the selected pos
-                unsigned short mask= 1<<i;
-                unsigned short new_computer_state=computer;
-                unsigned short new_player_state=player;
+                std::uint16_t mask= 1<<i;
+                std::uint16_t new_computer_state=computer;
+                std::uint16_t new_player_state=player;
                 Player_t next_player=Player_t::Computer;
                 if(actual_player==Player_t::Computer)//if computer was the last player
                 {
@@ -163,12 +165,12 @@ struct game_state
         }
         
     }
-    bool is_set_bit(const unsigned short value,const unsigned char bit_n) const 
+    bool is_set_bit(const std::uint16_t value,const std::uint8_t bit_n) const 
     {
         return  value&(1<<bit_n);
     }
 
-    Player_t get_player_movement(unsigned char n) const 
+    Player_t get_player_movement(std::uint8_t n) const 
     {
         if(is_set_bit(player,n))return Player_t::Human;
         else if(is_set_bit(computer,n))return Player_t::Computer;
@@ -178,40 +180,55 @@ struct game_state
     void overload_as_matrix(std::ostream & o) const
     {
         
-        for (unsigned char row=0;row<3;row++)
+        o<<"\n-------\n";
+        for (std::uint8_t row=0;row<3;row++)
         {
-            for (unsigned char col=0;col<3;col++)
+            o<<'|';
+            for (std::uint8_t col=0;col<3;col++)
             {
                 const Player_t p=get_player_movement(row*3+col);
                 if(p==Player_t::Computer)o<<'#';
                 else if(p==Player_t::Human) o<<'@';
-                else o<<'0';
-                o<<'\t';
+                else o<<' ';
                 //o<<value.get_player_movement(row*3+col)<<'\t';
+                o<<"|";
             }
-            o<<"\n";
+            o<<"\n-------\n";
         }
-        o<<"game stats\nWins: "<<win_computer<<"\nLoss: "<<win_human<<"\nTies: "<<tie<<'\n';
+        //o<<"game stats\nWins: "<<win_computer<<"\nLoss: "<<win_human<<"\nTies: "<<tie<<'\n';
+        //o<<"Score: "<<win_computer-win_human<<'\n';
         o<<"WINER: "<<winner<<"\n";
+        //o<<"Actual Player: "<<actual_player<<'\n';
+
 
     }
-     game_state optimal_movement()
+    game_state* optimal_movement()
     {
-        
-
-        return *std::min_element(
+        return &*std::max_element(
                 possible_game_states.begin(),
                 possible_game_states.end(),
-                [](const auto& a,const auto & b){
-                return (a.win_computer-a.win_human)-
-                       (b.win_computer-b.win_human);}
+                [](const auto& a,const auto & b){return a.get_score()<b.get_score();}
                 );
+    }
+    const double get_score()const 
+    {
+        return get_score(*this,1);
+        
+    }
+    const double get_score(const game_state&gs,const int level)const
+    {
+        if(gs.possible_game_states.empty())return gs.win_computer/level-gs.win_human;
+        double score=0;
+        for (const auto & pgs:gs.possible_game_states)
+        {
+            score+=get_score(pgs,level+1);
+        }
+        return score;
     }
 };
 
 std::ostream & operator<<(std::ostream& o, const game_state& value )
 {
-    o<<"--------game_state--------\n";
     value.overload_as_matrix(o);
 //    for (const auto & pgs:value.possible_game_states)pgs.overload_as_matrix(o);
     return o<<"\n\n";
